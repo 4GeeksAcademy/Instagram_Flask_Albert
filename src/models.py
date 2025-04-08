6309e9ea15dfddfc9802f4084b84acd531617d6e
Albert Pregonas <albertpregonas@gmail.com>
2025-04-08 09:07:53 +0000

    commit

--- src/models.py
+++ src/models.py
@@ -15,7 +15,7 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     username: Mapped[str] = mapped_column(String(120), unique=True)
-    fitsname: Mapped[str] = mapped_column(String(120))
+    firstname: Mapped[str] = mapped_column(String(120))
     lastname: Mapped[str] = mapped_column(String(120))
     password: Mapped[str] = mapped_column(String(120), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
